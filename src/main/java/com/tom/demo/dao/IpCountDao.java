916f53dbc1e954c3a498874d2bd7d9319bf975c4
tom.tang <tom.tang@sainstore.com>
2017-09-12 14:20:45 +0800

    aaa

--- src/main/java/com/tom/demo/dao/IpCountDao.java
+++ src/main/java/com/tom/demo/dao/IpCountDao.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by tom.tang on 2017/7/20.
  */
-public interface IpCountDao extends JpaRepository<IpCount, String> {
+public interface IpCountDao extends JpaRepository<IpCount, Long> {
 }
